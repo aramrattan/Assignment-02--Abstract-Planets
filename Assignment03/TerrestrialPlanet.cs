43c1eb42e62aa9fbbecf679af5d468ac5d16ead4
aramrattan <arlina.ramrattan@gmail.com>
2015-02-13 14:57:54 -0500

    TerrestrialPlanet made and constructor

---
+++ Assignment03/TerrestrialPlanet.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment03
+{
+    class TerrestrialPlanet : Planet
+    {
+        //Private instances
+        private bool _oxygen;
+
+        //constructor
+        public TerrestrialPlanet(string name, double diameter, double mass, bool oxygen) : base(name, diameter, mass)
+        {
+            this._oxygen = oxygen;
+        }
+    }
+}
